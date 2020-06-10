f5d7d85344b65ca557842fc0b5337aabd47207ac
john turingan <john.turingan@gmail.com>
2020-06-10 13:31:03 -0400

    Finalize Minimum viable Features

--- README.md
+++ README.md
@@ -1,2 +1,116 @@
-# fab-lemp-automation
-A Simple Web Server Setup Automation for LEMP Stack using Python
+# [Fab](http://www.fabfile.org/)-Lemp-Automation
+
+> Is a very Simple Web Server Setup Automation for LEMP Stack using Python Fabric. 
+> 
+> This will install all the necessary components and configure the important modules to get you started without having to manually do all the dirty stuff.
+
+
+## Installation :large_blue_circle:
+
+Simply clone the repository
+
+```
+git clone https://github.com/johnturingan/fab-lemp-automation.git
+```
+
+or download the zip file and extract it
+
+```
+https://github.com/johnturingan/fab-lemp-automation/archive/master.zip
+```
+##### PEM Folder
+> Inside the **pem** folder, you need to put the following:
+> 
+ * **Remote Server Private Key (.pem)** - Will use this to connect to your remote server. Make sure that your .pem permission is **0400**. Change it if not.
+ ``` $ chmod 0400 YourPrivateKey.pem```
+
+##### SSH Folder
+>  
+ *  **id_rsa** - Deploy user private key
+ *  **id_rsa.pub** - Deploy user public key
+ *  **authorized_keys** = public key of the users you want to have access to your remote server. Usually this is your personal public key
+
+##### NGINX Folder
+
+> Inside the nginx folder is where you need to put your nginx configuration of your setup. Check the template-cms for your reference
+
+##### ENV 
+
+> Make a copy of env.sample.json at name it **env.json**. Adjust any configuration based on what you need.
+
+## Usage :white_check_mark:
+
+After cloning or downloading, go to the folder and run the command
+
+```
+$ fab -i pem/project.pem -H=user@127.0.0.1 init
+```
+* **project.pem** - This is the private key to access your remote server
+* **user** - Change this to server's user account. (AWS default for Ubuntu Server is **ubuntu**)
+* **127.0.0.1** - Change this to remote server's ip address
+
+## What's Inside? :question::o::question:
+
+This script runs on your local machine and connect to your remote server via ssh and execute commands.
+
+Below is the simple bullet diagram about how this automation works. 
+
+> 
+* Deploy User - Create deploy user
+  * create .ssh folder
+  * copy```authorized_keys, id_rsa, id_rsa.pub``` from the **pem** folder and put it to **.ssh/** folder of your remote server
+* Update Repositories
+ * Add necessary sources then update and upgrade.
+* PHP Installation
+ * Includes the following 
+>	
+```
+	php7.4-fpm 
+	php7.4-common 
+	php7.4-mbstring 
+	php7.4-xmlrpc 
+	php7.4-soap 
+	php7.4-gd 
+	php7.4-xml 
+	php7.4-intl 
+	php7.4-mysql 
+	php7.4-cli 
+	php7.4-zip 
+	php7.4-curl 
+	php7.4-imagick 
+	php7.4-dev 
+	php7.4-imap 
+	php7.4-opcache 
+	memcached
+```
+>
+*  Nginx Installation
+*  MariaDB Installation
+*  Package Manager Installation
+ *  Install Yarn
+ *  Install Composer
+*  Setup Nginx 
+ *  create virtual host
+ *  setup initial root directory
+*  Restart All services
+
+
+###### LAST NOTE:
+
+Please feel free to use this simple automation and send me any comment or suggestions, file a bug or enhancments to make this project works for all of us.
+
+
+
+
+----
+**[MIT](LICENSE) LICENSE** <br>
+copyright &copy; 2020 Scripts and Pixels.
+
+
+
+
+
+
+
+
+
---
+++ fabfile.py
@@ -0,0 +1,203 @@
+"""
+Machine Enviroment Setup Automation
+Version 1.0
+usage: fab -i pem/project.pem -H=ubuntu@127.0.0.1 init
+"""
+
+
+import os
+from invoke import task
+import json
+
+local_path = os.path.realpath(os.getcwd())
+
+with open('env.json') as f:
+    env = json.load(f)
+
+webEnv = env['web_setup']
+
+
+
+@task
+def init(c):
+
+
+    try:
+        deploy_user(c)
+    except:
+        pass
+
+    update_repositories(c)
+
+    php_installation(c)
+    server_installation(c)
+    package_manager_installation(c)
+
+    setup_web_directory(c)
+    setup_nginx(c)
+
+    configure_services(c)
+
+    restart(c)
+
+
+
+@task
+def deploy_user(c):
+    banner('Preparing deploy user', 'header')
+
+    banner('Add deploy user')
+    c.sudo('adduser deploy')
+    c.sudo('usermod -aG sudo deploy')
+
+    banner('Creating .ssh folder in deploy user')
+    c.sudo('mkdir -p /home/deploy/.ssh')
+
+    banner('Uploading ssh keys from local to remote /tmp/folder')
+    c.put(local_path + '/ssh/authorized_keys', '/tmp/authorized_keys')
+    c.put(local_path + '/ssh/id_rsa', '/tmp/id_rsa')
+    c.put(local_path + '/ssh/id_rsa.pub', '/tmp/id_rsa.pub')
+
+    banner('Moving ssh keys to deploy .ssh folder')
+    c.sudo('mv /tmp/authorized_keys /home/deploy/.ssh/authorized_keys')
+    c.sudo('mv /tmp/id_rsa /home/deploy/.ssh/id_rsa')
+    c.sudo('mv /tmp/id_rsa.pub /home/deploy/.ssh/id_rsa.pub')
+
+    banner('Changing permission and owner')
+    c.sudo('chown deploy:deploy /home/deploy/.ssh/ -R')
+    c.sudo('chmod 0600 /home/deploy/.ssh/ -R')
+    c.sudo('chmod 0700 /home/deploy/.ssh/')
+
+    c.run("echo 'deploy ALL=(ALL:ALL) ALL' | sudo EDITOR='tee -a' visudo")
+
+@task
+def update_repositories(c):
+    banner('Updating Repositories', 'header')
+
+    banner('Downloading necessary sources')
+    c.run('curl -sL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh')
+    c.run('curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -')
+    c.sudo('echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list')
+    c.sudo('apt-get install software-properties-common')
+    c.sudo('apt-get update -y')
+    c.sudo('apt-get upgrade -y')
+
+@task
+def php_installation(c):
+    banner('Installing PHP', 'header')
+    c.sudo('apt install -y '
+           'php7.4-fpm '
+           'php7.4-common '
+           'php7.4-mbstring '
+           'php7.4-xmlrpc '
+           'php7.4-soap '
+           'php7.4-gd '
+           'php7.4-xml '
+           'php7.4-intl '
+           'php7.4-mysql '
+           'php7.4-cli '
+           'php7.4-zip '
+           'php7.4-curl '
+           'php7.4-imagick '
+           'php7.4-dev '
+           'php7.4-imap '
+           'php7.4-opcache '
+           'memcached')
+
+@task
+def server_installation(c):
+
+    banner('Installing Servers', 'header')
+    c.sudo('apt install -y nginx')
+    c.sudo('apt-get install -y mariadb-server')
+
+@task
+def package_manager_installation(c):
+    banner('Installing Package Managers', 'header')
+    c.sudo('bash nodesource_setup.sh ')
+    c.sudo('apt-get -y install yarn')
+    c.sudo('apt install -y composer')
+
+@task
+def setup_web_directory(c):
+    banner('Setting up web directories')
+    c.sudo('mkdir -p /srv/http/cms')
+    c.sudo('mkdir -p /srv/http/web')
+    c.sudo('mkdir -p /srv/http/api')
+    c.sudo('mkdir -p /srv/http/' + webEnv['type'] + '/' + webEnv['project_name'])
+    banner('Writing initial php file')
+    c.sudo('echo "Hello ' + webEnv['project_name'] + '" | sudo tee  /srv/http/' + webEnv['type'] + '/' + webEnv['project_name'] + '/index.php')
+    c.sudo('chown deploy:deploy /srv/ -R')
+
+@task
+def setup_nginx(c):
+
+    banner('Setting up NGINX', 'header')
+
+    banner('Create log files')
+    c.sudo('touch /var/log/nginx/' + webEnv['nginx']['log_name']['error'])
+    c.sudo('touch /var/log/nginx/' + webEnv['nginx']['log_name']['access'])
+
+    banner('Upload nginx site-available file to tmp')
+    print(local_path + webEnv['nginx']['template_path'] + webEnv['nginx']['template'])
+
+    c.put(
+        local_path + webEnv['nginx']['template_path'] + webEnv['nginx']['template'],
+        '/tmp/' + webEnv['nginx']['template']
+    )
+
+    print('cleaning sites-enabled to avoid error')
+    c.sudo('rm -rf /etc/nginx/sites-enabled/' + webEnv['nginx']['template'])
+
+    banner('Moving nginx file to sites-available')
+    c.sudo(
+        'mv /tmp/' + webEnv['nginx']['template'] + ' ' +
+        '/etc/nginx/sites-available/' + webEnv['nginx']['template']
+    )
+
+    banner('Creating symbolic link for site-enabled')
+    c.sudo('ln -s /etc/nginx/sites-available/' + webEnv['nginx']['template'] + ' /etc/nginx/sites-enabled/' + webEnv['nginx']['template'])
+    c.sudo('chown root:root /etc/nginx/sites-available/' + webEnv['nginx']['template'])
+
+@task
+def configure_services(c):
+
+    banner('Configuring Services')
+    banner('Configuring MariaDB ...')
+
+    c.sudo('sed -ie "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mariadb.conf.d/50-server.cnf')
+
+    banner('Configuring MariaDB Done')
+    banner('Configuring PHP ...')
+
+    c.sudo('sed -ie "s/.*user = .*/user = deploy/" /etc/php/7.4/fpm/pool.d/www.conf ')
+    c.sudo('sed -ie "s/.*group = .*/group = deploy/" /etc/php/7.4/fpm/pool.d/www.conf ')
+    c.sudo('sed -ie "s/.*listen = .*/listen = 127.0.0.1:9000/" /etc/php/7.4/fpm/pool.d/www.conf ')
+
+    banner('Configuring PHP Done')
+    banner('Configuring Nginx ...')
+
+    c.sudo('sed -ie "s/.*user .*/user deploy;/" /etc/nginx/nginx.conf ')
+
+    banner('Configuring Nginx Done')
+
+def restart(c):
+    banner('restarting services!')
+
+    c.sudo('service nginx restart')
+    c.sudo('service php7.4-fpm restart')
+    c.sudo('service mysql restart')
+
+
+
+def banner (message, type = 'message'):
+
+    if type == 'header':
+        print("\n")
+        print("************************************************")
+        print(message)
+        print("************************************************")
+        print("\n")
+    else:
+        print(">>>>>>>>>>  " + message + "  <<<<<<<<<<<")
+
